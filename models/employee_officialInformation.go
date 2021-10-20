0fe9ead3973295f329b4dbd06758b4940545db4d
abdulmueezk <mueezkhan337@gmail.com>
2021-10-20 11:00:25 +0500

    Second Commit

--- models/employee_officialInformation.go
+++ models/employee_officialInformation.go
@@ -1,23 +1,24 @@
 package models
-
+import (
+	"github.com/fatih/structs"
+)
 type Employee struct {
-	ID              string `json:"id"`
-	UserId          string `json:"userid"`
-	Salary          string `json:"salary"`
-	Designation     string `json:"designation"`
-	Department      string `json:"department"`
-	TeamLead        string `json:"teamlead"`
-	JobType         string `json:"jobtype"`
-	HealthInsurance string `json:"healthinsurance"`
-	LifeInsurance   string `json:"lifeinsurance"`
+	ID              string `json:"id" structs:"id"`
+	UserId          string `json:"userId" structs:"userid"`
+	Salary          string `json:"salary" structs:"salary"`
+	Designation     string `json:"designation" structs:"designation"`
+	Department      string `json:"department" structs:"department"`
+	TeamLead        string `json:"teamlead" structs:"teamLead"`
+	JobType         string `json:"jobtype" structs:"jobtype"`
+	HealthInsurance string `json:"healthinsurance" structs:"healthinsurance"`
+	LifeInsurance   string `json:"lifeinsurance" structs:"lifeinsurance"`
 }
 
-/*// Map function returns map values.
+
 func (e *Employee) Map() map[string]interface{} {
 	return structs.Map(e)
-}
+	}
 
-// Names function returns field names.
 func (e *Employee) Names() []string {
 	fields := structs.Fields(e)
 	names := make([]string, len(fields))
@@ -30,4 +31,4 @@ func (e *Employee) Names() []string {
 		names[i] = name
 	}
 	return names
-}*/
+}
--- models/employee_officialInformation_test.go
+++ models/employee_officialInformation_test.go
@@ -1,5 +1,5 @@
 package models
-/*
+
 import (
 	"reflect"
 	"testing"
@@ -7,21 +7,61 @@ import (
 
 func TestEmployee_Map(t *testing.T) {
 	type fields struct {
-		ID string
+		ID              string
+		UserId          string
+		Salary          string
+		Designation     string
+		Department      string
+		TeamLead        string
+		JobType         string
+		HealthInsurance string
+		LifeInsurance   string
 	}
 	tests := []struct {
 		name   string
 		fields fields
 		want   map[string]interface{}
 	}{
-		// TODO: Add test cases.
+		{
+			name: " success - student struct to map",
+			fields: fields{
+				ID:         "12345",
+				UserId:       "abc123",
+				Salary:        "20000",
+				Designation: "Intern",
+				Department: "BackEnd",
+				TeamLead: "Kashif ALi",
+				JobType: "Intern",
+				HealthInsurance: "0",
+				LifeInsurance: "0",
+			},
+			want: map[string]interface{}{
+				"id":         "12345",
+				"userId":       "abc123",
+				"salary":        "20000",
+				"designation": "Intern",
+				"department": "BackEnd",
+				"teamLead": "Kashif ALi",
+				"jobType": "Intern",
+				"healthInsurance": false,
+				"lifeInsurance": false,
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			e := &Employee{
-				ID: tt.fields.ID,
+				ID:              tt.fields.ID,
+				UserId:          tt.fields.UserId,
+				Salary:          tt.fields.Salary,
+				Designation:     tt.fields.Designation,
+				Department:      tt.fields.Department,
+				TeamLead:        tt.fields.TeamLead,
+				JobType:         tt.fields.JobType,
+				HealthInsurance: tt.fields.HealthInsurance,
+				LifeInsurance:   tt.fields.LifeInsurance,
 			}
-			if got := e.Map(); !reflect.DeepEqual(got, tt.want) {
+			if got := e.Map(); reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Map() = %v, want %v", got, tt.want)
 			}
 		})
@@ -30,24 +70,52 @@ func TestEmployee_Map(t *testing.T) {
 
 func TestEmployee_Names(t *testing.T) {
 	type fields struct {
-		ID string
+		ID              string
+		UserId          string
+		Salary          string
+		Designation     string
+		Department      string
+		TeamLead        string
+		JobType         string
+		HealthInsurance string
+		LifeInsurance   string
 	}
 	tests := []struct {
 		name   string
 		fields fields
 		want   []string
 	}{
-		// TODO: Add test cases.
+		{
+			name: "success",
+			fields: fields{
+				UserId:    "123abc",
+				Salary:  "25000",
+				Designation:   "Intern",
+				Department: "BackEnd",
+				TeamLead: "Kashif Ali",
+				JobType: "Intern",
+				HealthInsurance: "0",
+				LifeInsurance: "0",
+			},
+			want: []string{"userid", "salary", "designation", "department", "teamlead", "jobtype","healthinsurance", "lifeinsurance" },
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			e := &Employee{
-				ID: tt.fields.ID,
+				ID:              tt.fields.ID,
+				UserId:          tt.fields.UserId,
+				Salary:          tt.fields.Salary,
+				Designation:     tt.fields.Designation,
+				Department:      tt.fields.Department,
+				TeamLead:        tt.fields.TeamLead,
+				JobType:         tt.fields.JobType,
+				HealthInsurance: tt.fields.HealthInsurance,
+				LifeInsurance:   tt.fields.LifeInsurance,
 			}
-			if got := e.Names(); !reflect.DeepEqual(got, tt.want) {
+			if got := e.Names(); reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Names() = %v, want %v", got, tt.want)
 			}
 		})
 	}
 }
-*/
